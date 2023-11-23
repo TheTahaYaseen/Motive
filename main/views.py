0f8114d71e325264c520637fc21444f7856ab189
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-24 03:37:17 +0500

    The Standard Bit Of Template Implementation

--- main/views.py
+++ main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    context = {}
+    include_post_quote = True
+    context = {"include_post_quote": include_post_quote}
     return render(request, "home.html", context)
