cafaab8ba01bcc78f86e1c03057ec041cf18cd05
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-24 01:29:39 +0500

    The UserFlow Is This Simple

---
+++ pseduocode.py
@@ -0,0 +1,20 @@
+# UserFlow 
+
+"""
+
+Opens App:
+
+    Username -> Redirects To All Quotes Posted By You ->
+        Quotes Page ->
+            Shows All Your Quotes Sorted By Recency With Update And Delete Button For You
+    Post Quote Button -> Redirects To Post Page ->
+        If Not Authenticated Redirected To Login
+        Just Quote, By(Author) And Tags Input And Then, Redirected To Previous Page            
+    All Quotes Component ->
+        Shows All Quotes Sorted By Based On Engagement(Likes And Comments) -> Tags, Author And Quote
+        Upon Click On Author -> Redirected To Profile Page Of Quotes ->
+            Shows All Your Quotes Sorted By Recency With Update And Delete Button For You
+
+    For Every Quote (If Poster Is User, See Poster Options Too)
+             
+"""
