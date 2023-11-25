dea601a2c35d74adb99a4af2e76152f820147ebf
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-25 13:16:39 +0500

    I Won't Implement Likes And The Project Is Now Complete (What A Small Project It Is)

--- main/models.py
+++ main/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Like(models.Model):
-    by = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
 class Comment(models.Model):
     by = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField()
